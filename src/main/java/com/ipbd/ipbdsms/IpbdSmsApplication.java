2efc4b696cfde10db42a53acd4ec49a44c5306eb
LiBai <LiBai@qq.com>
2023-11-06 20:45:39 +0800

    初始化

---
+++ src/main/java/com/ipbd/ipbdsms/IpbdSmsApplication.java
@@ -0,0 +1,13 @@
+package com.ipbd.ipbdsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpbdSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpbdSmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipbd/ipbdsms/IpbdSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipbd.ipbdsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpbdSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
